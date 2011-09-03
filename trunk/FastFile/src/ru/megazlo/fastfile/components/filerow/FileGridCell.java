d04f1c3bd16606e6d20223085059d42be7a44de4
megazlonet@gmail.com <megazlonet@gmail.com@85a86685-b049-b641-2327-2a30ad17d28c>
2011-09-03 08:57:04 +0000

    Пререлиз ff и fn

--- trunk/FastFile/src/ru/megazlo/fastfile/components/filerow/FileGridCell.java
+++ trunk/FastFile/src/ru/megazlo/fastfile/components/filerow/FileGridCell.java
@@ -1,19 +1,17 @@
 package ru.megazlo.fastfile.components.filerow;
 
 import android.content.Context;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class FileGridCell extends LinearLayout {
 
-	private TextView m_txt_dir;
-	private ImageView m_icon, m_check;
-	private FileRowData dat;
+	// private TextView m_txt_dir;
+	// private ImageView m_icon, m_check;
+	// private FileRowData dat;
 
 	public FileGridCell(Context context, FileRowData data) {
 		super(context);
-		dat = data;
+		// dat = data;
 		initChild(context, data);
 	}
 
